cc255648ced63223662fef208e92519f5a05b356
Streit Sándor <sandorstreit@gmail.com>
2025-03-20 22:54:44 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import icecream as ic
+import matplotlib.pyplot as plt
+
+
+# Prioritásos sor osztály
+class Class1:
+    def __init__(self):
+        return
+
+
+# Ritka mátrix osztály
+class Class2:
+    def __init__(self):
+        return
+
+
+# Alsó háromszög mátrix osztály
+class Class3:
+    def __init__(self):
+        return
+
+
+# Program belépése
+if __name__ == "__main__":
+    # Class1
+    # Class2
+    # Class3
+    teszt = Class1
