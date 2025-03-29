6bd2d66a6a1c49cffc4b1a4a5990e28108df17dc
molnarp <molnarpatrik11@gmail.com>
2025-03-29 11:45:34 +0100

    teszt delete, class 2 kód hozzáadás

--- main.py
+++ main.py
@@ -9,22 +9,68 @@ class Class1:
     def __init__(self):
         return
 
-
 # Ritka mátrix osztály
-class Class2:
-    def __init__(self):
-        return
+class SparseMatrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = {}
 
+    def insert(self, row, col, value):
+        if value != 0:
+            self.data[(row, col)] = value
+
+    def get(self, row, col):
+        return self.data.get((row, col), 0)
+
+    def delete(self, row, col):
+        if (row, col) in self.data:
+            del self.data[(row, col)]
+
+    def display(self):
+        matrix = np.zeros((self.rows, self.cols))
+        for (r, c), v in self.data.items():
+            matrix[r, c] = v
+        print(matrix)
+
+    def visualize(self):
+        matrix = np.zeros((self.rows, self.cols))
+        for (r, c), v in self.data.items():
+            matrix[r, c] = v
+
+        plt.figure(figsize=(6, 6))
+        plt.imshow(matrix, cmap="Blues", interpolation="none")
+        plt.colorbar()
+        plt.title("Ritka mátrix vizualizáció")
+        plt.show()
+
+    @staticmethod
+    def from_csv(file_path):
+        df = pd.read_csv(file_path, header=None)
+        matrix = df.to_numpy()
+        total_elements = matrix.size
+        zero_elements = np.count_nonzero(matrix == 0)
+
+        if zero_elements / total_elements > 0.5:  # Ritka mátrix ellenőrzés
+            sparse_matrix = SparseMatrix(*matrix.shape)
+            for r in range(matrix.shape[0]):
+                for c in range(matrix.shape[1]):
+                    if matrix[r, c] != 0:
+                        sparse_matrix.insert(r, c, matrix[r, c])
+            return sparse_matrix
+        else:
+            print("Nem ritka mátrix.")
+            return None
 
 # Alsó háromszög mátrix osztály
 class Class3:
     def __init__(self):
         return
 
-
 # Program belépése
 if __name__ == "__main__":
-    # Class1
-    # Class2
-    # Class3
-    teszt = Class1
+    print("\n--- Ritka mátrix teszt (CSV fájlból) ---")
+    sm = SparseMatrix.from_csv("import/matrix.csv")
+    if sm:
+        sm.display()
+        sm.visualize()
